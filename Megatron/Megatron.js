0a2b56dfe5873d098fcf21369ca5cdc0f147ffbf
kausahecker <kaushikpr23@gmail.com>
2024-12-16 20:31:41 +0530

    Delete Megatron directory

--- Megatron/Megatron.js
+++
@@ -1,8 +0,0 @@
-function uncover()
-{
-    if(document.getElementById("aud").paused)
-    {
-        document.getElementById("aud").play();
-    }
-    document.getElementById("cover").className="uncovered";
-}
